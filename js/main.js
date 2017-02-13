cf86a9a3c2440e7601fe571a0e3eb39e81f7345f
cosmegt <caneko@live.com>
2017-02-12 17:26:01 -0800

    done with projects nav functionality

--- js/main.js
+++ js/main.js
@@ -25,32 +25,53 @@ var nClose= function(elementIDc, iElementIDc){
     $(elementIDc).delay(closeTime).fadeIn(openTime);
     $(iElementIDc).delay(delayTime).fadeOut(openTime);
 }
-
+var oMenu= function(menuID){
+    $(menuID).animate({
+        left: '120px'
+    });
+}
+var cMenu= function(menuIDc){
+    $(menuIDc).animate({
+        left: '-500px'
+    });
+}
 //Actions
 $('#pImage').click(function(){
     nOpen('#pImage', '#cProjects');
     nClose('#tImage', '#cTinker');
     nClose('#mImage', '#cMore');
+    oMenu('pMenu');
+    cMenu('tMenu');
+    cMenu('mMenu');
 });
 
 $('#tImage').click(function(){
     nOpen('#tImage', '#cTinker');
     nClose('#pImage', '#cProjects');
     nClose('#mImage', '#cMore');
+    oMenu('tMenu');
+    cMenu('pMenu');
+    cMenu('mMenu');
 });
 
 $('#mImage').click(function(){
     nOpen('#mImage', '#cMore');
     nClose('#tImage', '#cTinker');
     nClose('#pImage', '#cProjects');
+    oMenu('mMenu');
+    cMenu('tMenu');
+    cMenu('pMenu');
 });
 
 $('#cProjects').click(function(){
     nClose('#pImage', '#cProjects');
+    cMenu('pMenu');
 })
 $('#cTinker').click(function(){
     nClose('#tImage', '#cTinker');
+    cMenu('tMenu');
 })
 $('#cMore').click(function(){
     nClose('#mImage', '#cMore');
+    cMenu('mMenu');
 })
